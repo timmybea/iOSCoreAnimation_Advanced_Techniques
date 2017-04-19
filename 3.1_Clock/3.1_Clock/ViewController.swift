86050a54e52bda6c7dd95a46a707ee9dd23fd5d3
Tim Beals <tim.beals@gmail.com>
2017-04-18 21:15:34 -0700

    calculate angle of clock hands based on Timer and animate movement with affine

--- 3.1_Clock/3.1_Clock/ViewController.swift
+++ 3.1_Clock/3.1_Clock/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController {
     let secondsHand: UIImageView = {
         let view = UIImageView()
         view.image = UIImage(named: "second_hand")
-        view.contentMode = .scaleAspectFit
+        view.contentMode = .scaleAspectFill
         return view
     }()
     
@@ -61,12 +61,16 @@ class ViewController: UIViewController {
         
         hoursHand.frame = CGRect(x: 0, y: 0, width: 6, height: 122)
         hoursHand.center = view.center
-
+        hoursHand.layer.anchorPoint = CGPoint(x: 0.5, y: 0.9)
+        
         minutesHand.frame = CGRect(x: 0, y: 0, width: 3, height: 122)
         minutesHand.center = view.center
+        minutesHand.layer.anchorPoint = CGPoint(x: 0.5, y: 0.9)
 
         secondsHand.frame = CGRect(x: 0, y: 0, width: 3, height: 144)
         secondsHand.center = view.center
+        secondsHand.layer.anchorPoint = CGPoint(x: 0.5, y: 0.9)
+        
     }
     
     func setupTimer() {
@@ -75,7 +79,29 @@ class ViewController: UIViewController {
     
     
     func tick() {
-        print("Hello")
+        
+        //http://stackoverflow.com/questions/38248941/how-to-get-time-hour-minute-second-in-swift-3-using-nsdate
+        let date = NSDate()
+        
+        var calendar = NSCalendar.current
+        
+        let unitFlags = Set<Calendar.Component>([.hour, .minute, .second])
+        calendar.timeZone = TimeZone(identifier: "America/Los_Angeles")!
+        
+        let components = calendar.dateComponents(unitFlags, from: date as Date)
+        
+        //Calculate hands angle
+
+        let hoursAngle: CGFloat = CGFloat((Double(components.hour!) / 12.0) * Double.pi * 2.0)
+     
+        let minsAngle: CGFloat = CGFloat((Double(components.minute!) / 60.0) * Double.pi * 2.0)
+        
+        let secsAngle: CGFloat = CGFloat((Double(components.second!) / 60.0) * Double.pi * 2.0)
+        
+        self.hoursHand.transform = CGAffineTransform(rotationAngle: hoursAngle)
+        self.minutesHand.transform = CGAffineTransform(rotationAngle: minsAngle)
+        self.secondsHand.transform = CGAffineTransform(rotationAngle: secsAngle)
+        
     }
 
     override func didReceiveMemoryWarning() {
