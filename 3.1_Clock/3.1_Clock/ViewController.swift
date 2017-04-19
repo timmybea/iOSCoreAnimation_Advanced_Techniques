69df5e82b5ce071381d7fe5ccba0b16b1ff2dd5c
Tim Beals <tim.beals@gmail.com>
2017-04-18 17:28:31 -0700

    setup views and timer for clock face animation

--- 3.1_Clock/3.1_Clock/ViewController.swift
+++ 3.1_Clock/3.1_Clock/ViewController.swift
@@ -17,6 +17,28 @@ class ViewController: UIViewController {
         return view
     }()
     
+    let secondsHand: UIImageView = {
+        let view = UIImageView()
+        view.image = UIImage(named: "second_hand")
+        view.contentMode = .scaleAspectFit
+        return view
+    }()
+    
+    let minutesHand: UIImageView = {
+        let view = UIImageView()
+        view.image = UIImage(named: "minute_hand")
+        view.contentMode = .scaleAspectFit
+        return view
+    }()
+    
+    let hoursHand: UIImageView = {
+        let view = UIImageView()
+        view.image = UIImage(named: "hour_hand")
+        view.contentMode = .scaleAspectFit
+        return view
+    }()
+    
+    var timer: Timer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,18 +46,36 @@ class ViewController: UIViewController {
         view.backgroundColor = .lightGray
         
         setupViews()
+        setupTimer()
     }
     
     
     func setupViews() {
         view.addSubview(clockFace)
-        
+        view.addSubview(hoursHand)
+        view.addSubview(minutesHand)
+        view.addSubview(secondsHand)
         
         clockFace.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
         clockFace.center = view.center
+        
+        hoursHand.frame = CGRect(x: 0, y: 0, width: 6, height: 122)
+        hoursHand.center = view.center
 
+        minutesHand.frame = CGRect(x: 0, y: 0, width: 3, height: 122)
+        minutesHand.center = view.center
+
+        secondsHand.frame = CGRect(x: 0, y: 0, width: 3, height: 144)
+        secondsHand.center = view.center
+    }
+    
+    func setupTimer() {
+        self.timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(tick), userInfo: nil, repeats: true)
+    }
     
     
+    func tick() {
+        print("Hello")
     }
 
     override func didReceiveMemoryWarning() {
